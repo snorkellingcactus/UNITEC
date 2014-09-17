1e8d25a4794ecd7adf3b5cdea467da15864f2a94
Gonzalo García <snorkellingcactus@gmail.com>
2014-09-17 00:47:58 -0300

    -Limpieza y optimización de código.

--- seccs/organigrama.php
+++ seccs/organigrama.php
@@ -1,126 +1,45 @@
 <!--	:::::::::Organigrama:::::::::	-->
-<!--<section id="labs">
-	<table>
-		<thead>
-			<tr>
-				<td colspan="6">
-					<div class="organicaja">
-						<p>UNITEC</p>
-					</div>
-				</td>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td colspan="2"><img src="./img/flecha0.png" alt="flecha" /></td>
-				<td colspan="2"><img src="./img/flecha3.png" alt="flecha" /></td>
-				<td colspan="2"><img src="./img/flecha5.png" alt="flecha" /></td>
-			</tr>
-			<tr>
-				<td colspan="2">
-					<div class="organicaja">
-						<p>Espacio Desarrollo tecnologías</p>
-					</div>
-				</td>
-				<td colspan="2">
-					<div class="organicaja">
-						<p>Espacio Desarrollo tecnologías</p>
-					</div>
-				</td>
-				<td colspan="2">
-					<div class="organicaja">
-						<p>Espacio Educación ingeniería</p>
-					</div>
-				</td>
-			</tr>
-		</tbody>
-		<tfoot>
-			<tr>
-				<td><img src="./img/flecha0.png" alt="flecha" /></td>
-				<td><img src="./img/flecha1.png" alt="flecha" /></td>
-				<td><img src="./img/flecha2.png" alt="flecha" /></td>
-				<td><img src="./img/flecha3.png" alt="flecha" /></td>
-				<td><img src="./img/flecha4.png" alt="flecha" /></td>
-				<td><img src="./img/flecha5.png" alt="flecha" /></td>
-			</tr>
-			<tr>
-				<td>
-					<div  class="organicaja">
-						<p>LATE</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>LABTIC</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>EDETEC</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>LEMA</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>LITAE</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>CALED</p>
-					</div>
-				</td>
-			</tr>
-		</tfoot>
-	</table>
-</section>-->
 <section id="labs">
 
 <div class"container-fluid">
-
-<div class="row">
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"">
-	<div class="organicaja1" > <h2>UNITEC</h2> </div>
-</div>
-</div>
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"">
+			<div class="organicaja" > <h2>UNITEC</h2> </div>
+		</div>
 	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-		<div class="organicaja2">Espacios De Extención</div>
+		<div class="organicaja red">Espacios De Extención</div>
 			<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja2">LATE</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja red">LATE</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja red">LABTIC</div>
+				</div>
 			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja2">LABTIC</div>
+	</div>
+	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
+		<div class="organicaja gold">Espacio Desarrollo tecnologías</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja gold">EDETEC</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja gold">LEMA</div>
+				</div>
+			</div>
+	</div>
+	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
+		<div class="organicaja green">Espacio Educación ingeniería</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja green">LITAE</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja green">CALED</div>
+				
 			</div>
 		</div>
 	</div>
-	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
-		<div class="organicaja3">Espacio Desarrollo tecnologías</div>
-			<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja3">EDETEC</div>
-			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja3">LEMA</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
-		<div class="organicaja4">Espacio Educación ingeniería</div>
-			<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja4">LITAE</div>
-			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja4">CALED</div>
-			</div>
-			</div>
-	</div>
-
 </div>
 
 </section>
