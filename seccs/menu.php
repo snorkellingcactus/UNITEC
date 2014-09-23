c2668c4362d85ca8ba3da902806f97dd3694b027
Gonzalo García <snorkellingcactus@gmail.com>
2014-09-23 17:48:51 -0300

    Algunas correcciones

--- seccs/menu.php
+++ seccs/menu.php
@@ -1,9 +1,9 @@
 <div class='menuSup visible-xs'>
 		<a href="./index.php?OpcSel=0#sobre"<?php resaltaOpcN(0) ;?> >Inicio</a>
-		<a href="./index.php?OpcSel=1"		<?php resaltaOpcN(1) ;?> >Novedades</a>
+		<a href="./index.php?OpcSel=1#nov"		<?php resaltaOpcN(1) ;?> >Novedades</a>
 		<a href="./index.php?OpcSel=2#labs"	<?php resaltaOpcN(2) ;?> >Espacios de extensión</a>
 		<a href="./index.php?OpcSel=3#cal"	<?php resaltaOpcN(3) ;?> >Eventos</a>
-		<a href="./index.php?OpcSel=4"		<?php resaltaOpcN(4) ;?> >Galería</a>
+		<a href="./index.php?OpcSel=4#gal"		<?php resaltaOpcN(4) ;?> >Galería</a>
 		<a href="./index.php?OpcSel=5"		<?php resaltaOpcN(5) ;?> >Idioma</a>
 		<a href="./index.php?OpcSel=6"		<?php resaltaOpcN(6) ;?> >Accesibilidad</a>
 		<a href="./inicio_sesion.php"		>Iniciar Sesión</a>
@@ -26,7 +26,7 @@
 					<a href="./index.php?OpcSel=3#cal"		<?php resaltaOpcN(3) ;?> >Eventos</a>
 				</li>
 				<li>
-					<a href="./index.php?OpcSel=4"		<?php resaltaOpcN(4) ;?> >Galería</a>
+					<a href="./index.php?OpcSel=4#gal"		<?php resaltaOpcN(4) ;?> >Galería</a>
 				</li>
 				<li>
 					<a href="./index.php?OpcSel=5"		<?php resaltaOpcN(5) ;?> >Idioma</a>
