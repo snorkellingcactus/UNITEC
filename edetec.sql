e14c7175b4656153254cab08f1ce01a5566bad2c
root <root@linux-7c3y.site>
2014-09-25 02:11:05 -0300

    -Improvisaciones sobre el organigrama de emiliano. -Actualizada la base de datos.

---
+++ edetec.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 25-09-2014 a las 07:06:42
+-- Versión del servidor: 5.6.16
+-- Versión de PHP: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `edetec`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Comentarios`
+--
+
+CREATE TABLE IF NOT EXISTS `Comentarios` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `IP` int(11) DEFAULT NULL,
+  `Usuario` int(11) DEFAULT NULL,
+  `Contenido` int(11) DEFAULT NULL,
+  `Baneado` bit(1) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `Usuario` (`Usuario`),
+  KEY `Contenido` (`Contenido`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Contenido`
+--
+
+CREATE TABLE IF NOT EXISTS `Contenido` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Contenido` text COLLATE utf8_unicode_ci,
+  `Lenguaje` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Imagenes`
+--
+
+CREATE TABLE IF NOT EXISTS `Imagenes` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Url` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Ancho` int(11) DEFAULT NULL,
+  `Alto` int(11) DEFAULT NULL,
+  `Alt` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Titulo` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Contenido` int(11) DEFAULT NULL,
+  `Comentarios` int(11) DEFAULT NULL,
+  `Lenguaje` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `Contenido` (`Contenido`),
+  KEY `Comentarios` (`Comentarios`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `Usuarios` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `NombreUsuario` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Contrasena` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Email` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Baneado` bit(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Comentarios`
+--
+ALTER TABLE `Comentarios`
+  ADD CONSTRAINT `Comentarios_ibfk_1` FOREIGN KEY (`Usuario`) REFERENCES `Usuarios` (`ID`),
+  ADD CONSTRAINT `Comentarios_ibfk_2` FOREIGN KEY (`Contenido`) REFERENCES `Contenido` (`ID`);
+
+--
+-- Filtros para la tabla `Imagenes`
+--
+ALTER TABLE `Imagenes`
+  ADD CONSTRAINT `Imagenes_ibfk_1` FOREIGN KEY (`Contenido`) REFERENCES `Contenido` (`ID`),
+  ADD CONSTRAINT `Imagenes_ibfk_2` FOREIGN KEY (`Comentarios`) REFERENCES `Comentarios` (`ID`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- seccs/organigrama.php
+++ seccs/organigrama.php
@@ -1,146 +1,41 @@
-<!--	:::::::::Organigrama:::::::::	-->
-<!--<section id="labs">
-	<table>
-		<thead>
-			<tr>
-				<td colspan="6">
-					<div class="organicaja">
-						<p>UNITEC</p>
-					</div>
-				</td>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td colspan="2"><img src="./img/flecha0.png" alt="flecha" /></td>
-				<td colspan="2"><img src="./img/flecha3.png" alt="flecha" /></td>
-				<td colspan="2"><img src="./img/flecha5.png" alt="flecha" /></td>
-			</tr>
-			<tr>
-				<td colspan="2">
-					<div class="organicaja">
-						<p>Espacio Desarrollo tecnologías</p>
-					</div>
-				</td>
-				<td colspan="2">
-					<div class="organicaja">
-						<p>Espacio Desarrollo tecnologías</p>
-					</div>
-				</td>
-				<td colspan="2">
-					<div class="organicaja">
-						<p>Espacio Educación ingeniería</p>
-					</div>
-				</td>
-			</tr>
-		</tbody>
-		<tfoot>
-			<tr>
-				<td><img src="./img/flecha0.png" alt="flecha" /></td>
-				<td><img src="./img/flecha1.png" alt="flecha" /></td>
-				<td><img src="./img/flecha2.png" alt="flecha" /></td>
-				<td><img src="./img/flecha3.png" alt="flecha" /></td>
-				<td><img src="./img/flecha4.png" alt="flecha" /></td>
-				<td><img src="./img/flecha5.png" alt="flecha" /></td>
-			</tr>
-			<tr>
-				<td>
-					<div  class="organicaja">
-						<p>LATE</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>LABTIC</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>EDETEC</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>LEMA</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>LITAE</p>
-					</div>
-				</td>
-				<td>
-					<div  class="organicaja">
-						<p>CALED</p>
-					</div>
-				</td>
-			</tr>
-		</tfoot>
-	</table>
-</section>-->
 <section id="labs">
-
-<div class"container-fluid" >
-
-<div class="row">
-
-		<h1 class="hidden-xs letra1" style="margin:5% 0 5% 0; padding:2% font-size:50%"> Organigrama </h1>
-		<h1 class="visible-xs letra2" style="margin:5% 0 5% 0; padding:2% font-size:50%"> Organigrama </h1>
-
-	</div>
-
-<div class="row">
-<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-	<div class="organicaja1" > <h1>UNITEC</h1> </div>
-</div>
-<div class="row">
-<div class="hidden-xs col-sm-12 col-md-12 col-lg-12">
-	<img src="./img/linea1_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" />
-</div>
-</div>
-	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-		<div class="organicaja2"><h3>Espacios de Extención Principales</h3></div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" />
-			</div>
-			<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja2"><h4>LATE</h4></div>
-			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja2"><h4>LABTIC</h4></div>
-			</div>
+	<h1 class="hidden-xs letra1" style="margin:5% 0 5% 0; padding:2% font-size:50%"> Organigrama </h1>
+	<h1 class="visible-xs letra2" style="margin:5% 0 5% 0; padding:2% font-size:50%"> Organigrama </h1>
+	<div class="hidden-xs hidden-sm col-md-3 col-lg-3"></div>
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+		<div class="organicaja1" >
+			<h1>UNITEC</h1>
 		</div>
 	</div>
-	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
-		<div class="organicaja3"><h3>Espacios de Desarrollo Tecnológico</h3></div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" />
-			</div>
-			<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja3"><h4>EDETEC</h4></div>
-			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja3"><h4>LEMA</h4></div>
-			</div>
+	<img src="./img/linea1_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="hidden-xs hidden-sm col-md-12 col-lg-12"/>
+	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+		<h3 class="organicaja2">Espacios de Extensión Principales</h3>
+		<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"/>
+		<div class="organicaja2 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<h4>LATE</h4>
+		</div>
+		<div class="organicaja2 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<h4>LABTIC</h4>
+		</div>
+	</div>	
+	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+		<h3 class="organicaja3">Espacios de Desarrollo Tecnológico</h3>
+		<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"/>
+		<div class="organicaja3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<h4>EDETEC</h4>
+		</div>
+		<div class="organicaja3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<h4>LEMA</h4>
 		</div>
 	</div>
-	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
-		<div class="organicaja4"><h3>Espacios de Educación Ingenieril</h3></div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" />
-			</div>
-			<div class="row">
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja4"><h4>LITAE</h4></div>
-			</div>
-			<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-				<div class="organicaja4"><h4>CALED</h4></div>
-			</div>
-			</div>
+	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+		<h3 class="organicaja4">Espacios de Educación Ingenieril</h3>
+		<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"/>
+		<div class="organicaja4 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<h4>LITAE</h4>
+		</div>
+		<div class="organicaja4 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+			<h4>CALED</h4>
+		</div>
 	</div>
-
-</div>
-
 </section>
