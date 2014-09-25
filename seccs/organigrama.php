fd64fdc1cb2f2435ead455518e1906fb2ff3610e
Gonzalo García <snorkellingcactus@gmail.com>
2014-09-25 02:22:22 -0300

    El organigrama original de emiliano se ve mejor

--- seccs/organigrama.php
+++ seccs/organigrama.php
@@ -1,41 +1,45 @@
+<!--	:::::::::Organigrama:::::::::	-->
 <section id="labs">
-	<h1 class="hidden-xs letra1" style="margin:5% 0 5% 0; padding:2% font-size:50%"> Organigrama </h1>
-	<h1 class="visible-xs letra2" style="margin:5% 0 5% 0; padding:2% font-size:50%"> Organigrama </h1>
-	<div class="hidden-xs hidden-sm col-md-3 col-lg-3"></div>
-	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-		<div class="organicaja1" >
-			<h1>UNITEC</h1>
+
+<div class"container-fluid">
+	<div class="row">
+		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"">
+			<div class="organicaja" > <h2>UNITEC</h2> </div>
+		</div>
+	<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+		<div class="organicaja red">Espacios De Extención</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja red">LATE</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja red">LABTIC</div>
+				</div>
+			</div>
+	</div>
+	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
+		<div class="organicaja gold">Espacio Desarrollo tecnologías</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja gold">EDETEC</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja gold">LEMA</div>
+				</div>
+			</div>
+	</div>
+	<div class="col-xs-12 col-sm-4 col-md 4 col-lg-4">
+		<div class="organicaja green">Espacio Educación ingeniería</div>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja green">LITAE</div>
+				</div>
+				<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+					<div class="organicaja green">CALED</div>
+				
+			</div>
 		</div>
 	</div>
-	<img src="./img/linea1_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="hidden-xs hidden-sm col-md-12 col-lg-12"/>
-	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-		<h3 class="organicaja2">Espacios de Extensión Principales</h3>
-		<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"/>
-		<div class="organicaja2 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<h4>LATE</h4>
-		</div>
-		<div class="organicaja2 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<h4>LABTIC</h4>
-		</div>
-	</div>	
-	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-		<h3 class="organicaja3">Espacios de Desarrollo Tecnológico</h3>
-		<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"/>
-		<div class="organicaja3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<h4>EDETEC</h4>
-		</div>
-		<div class="organicaja3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<h4>LEMA</h4>
-		</div>
-	</div>
-	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-		<h3 class="organicaja4">Espacios de Educación Ingenieril</h3>
-		<img src="./img/linea2_organigrama.png" style="width:100%" alt="linea de interconeccion del organigrama" class="col-xs-12 col-sm-12 col-md-12 col-lg-12"/>
-		<div class="organicaja4 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<h4>LITAE</h4>
-		</div>
-		<div class="organicaja4 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-			<h4>CALED</h4>
-		</div>
-	</div>
+</div>
+
 </section>
