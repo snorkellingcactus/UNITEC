e83d540172035bc1fa8dff27dfefd4120dc6b69b
Gonzalo García <snorkellingcactus@gmail.com>
2015-04-08 12:40:28 -0300

    Cambiadas las latitudes del mapa

--- seccs/mapa.php
+++ seccs/mapa.php
@@ -7,14 +7,29 @@
 <script>
 	var map;
 
-	function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
+	function initialize() 
+  {
+
+    var pos=new google.maps.LatLng(-34.90693 , -57.94290);
+    var mapOptions = 
+    {
+      zoom: 8,
+      center: pos
+    };
+    map = new google.maps.Map( document.getElementById('map-canvas') , mapOptions );
+
+    var marcador=new google.maps.Marker
+    (
+      {
+        position: pos,
+        map:map,
+        title:'Departamento de Electrotecnia'
+      }
+    );
+
+    map.setCenter(pos);
+    map.setZoom(17)
+  }
 
 google.maps.event.addDomListener(window, 'load', initialize);
 </script>
