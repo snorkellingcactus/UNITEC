0c3d65b1ab1e18d20838460b212adf2b2d12de6a
Nadal Gonzalo Garcìa Zavala <snorkellingcactus@gmail.com>
2015-06-08 21:11:19 +0000

    Incluida la sección contacte

---
+++ php/procesa_mail.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML >
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="Página principal Unitec." />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<h1>Muchas gracias, su consulta fué enviada.</h1>
+		<?php
+			mail
+			(
+				'snorkellingcactus@gmail.com',
+				$_POST['Asunto'],
+				"Correo del consultante:\n".$_POST['Mensaje'],
+				'From: garciazavalanadal@gmail.com'
+			);
+		?>
+	</body>
+</html>
---
+++ seccs/contacto.php
@@ -0,0 +1,19 @@
+<div class="clearfix"></div>
+<form action="php/procesa_mail.php" method="POST" target="_blank" class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+	<label for="Correo" class="col-xs-12 col-sm-12 col-md-4 col-lg-4">Correo:</label>
+	<input type="text" name="Correo" id="Correo" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+	<div class="clearfix"></div>
+
+	<label for="Asunto" class="col-xs-12 col-sm-12 col-md-4 col-lg-4">Asunto:</label>
+	<input type="text" name="Asunto" id="Asunto" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+	<div class="clearfix"></div>
+
+	<label for="Mensaje" class="col-xs-12 col-sm-12 col-md-4 col-lg-4">Mensaje:</label>
+	<textarea name="Mensaje" id="Mensaje" class="col-xs-12 col-sm-12 col-md-6 col-lg-6"></textarea>
+
+	<div class="clearfix"></div>
+
+	<input type="submit" value="OK" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+</form>
