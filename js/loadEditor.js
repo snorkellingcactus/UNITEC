58959d72e5d9dac294554b8d6f8a218014b1ccab
Gonzalo García <snorkellingcactus@gmail.com>
2015-08-19 11:02:40 -0300

    Faltaba un archivo

---
+++ js/loadEditor.js
@@ -0,0 +1,28 @@
+head.ready
+(
+	function()
+	{
+		CKEDITOR.editorConfig=function(cfg)
+		{
+			cfg.customConfig='';
+			cfg.entities=false;
+		}
+/*
+		CKEDITOR.replaceAll
+		(
+			'ckeditorjs',
+			function(nEditor , config)
+			{
+				window.console.log('Hello');
+				nEditor.insertHtml(nEditor.element.innerHTML);
+				nEditor.element.innerHTML='';
+
+				config.customConfig='';
+				config.entities=false;
+
+				return true;
+			}
+		);
+*/
+	}
+);
